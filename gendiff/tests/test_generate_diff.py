2da74ab814b1c339db938d9e104a5333dc925765
Irina Nechetnaya <nechetnaya@gmail.com>
2023-07-17 16:27:18 +0300

    update tests

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -10,7 +10,7 @@ def test_generate_diff():
 def test_generate_diff_same():
     result = open("gendiff/tests/fixtures/result_same.txt").read()
     assert generate_diff("gendiff/tests/fixtures/file1.json",
-                         "gendiff/tests/fixtures/file1_1.json") == result
+                         "gendiff/tests/fixtures/file1.json") == result
 
 
 def test_generate_diff_one_empty():
@@ -22,4 +22,4 @@ def test_generate_diff_one_empty():
 def test_generate_diff_both_empty():
     result = open("gendiff/tests/fixtures/result_both_empty.txt").read()
     assert generate_diff("gendiff/tests/fixtures/empty_file.json",
-                         "gendiff/tests/fixtures/empty_file_2.json") == result
+                         "gendiff/tests/fixtures/empty_file.json") == result
