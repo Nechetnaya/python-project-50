646b6ebc7f4e48b13da12bd6893726bee6807d27
Irina Nechetnaya <nechetnaya@gmail.com>
2023-07-17 17:56:41 +0300

    add pyYML

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -2,15 +2,19 @@ from gendiff.modules.generate_diff import generate_diff
 import pytest
 
 fixtures_dir = "gendiff/tests/fixtures/"
+# general func job
 normal = ((f"{fixtures_dir}file1.json",
           f"{fixtures_dir}file2.json"),
           f"{fixtures_dir}expected_result.txt")
+# files are similar
 same = ((f"{fixtures_dir}file1.json",
         f"{fixtures_dir}file1.json"),
         f"{fixtures_dir}result_same.txt")
+# one file is empty
 one_empty = ((f"{fixtures_dir}file1.json",
              f"{fixtures_dir}empty_file.json"),
              f"{fixtures_dir}/result_one_empty.txt")
+# both files are empty
 both_empty = ((f"{fixtures_dir}empty_file.json",
               f"{fixtures_dir}empty_file.json"),
               f"{fixtures_dir}result_both_empty.txt")
