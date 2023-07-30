8e5c6f2ddec6d2c193016dc01396a8093758e551
Irina Nechetnaya <nechetnaya@gmail.com>
2023-07-30 18:23:27 +0300

    add format options

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -13,12 +13,13 @@ def main():
         )
     parser.add_argument('first_file')  # positional argument
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', default=stylish,
+    parser.add_argument('-f', '--format', default="stylish",
                         help='output format (default: "stylish")')
     args = parser.parse_args()
     file1 = args.first_file
     file2 = args.second_file
-    print(generate_diff(file1, file2))
+    format_ = args.format
+    print(generate_diff(file1, file2, format_))
 
 
 if __name__ == '__main__':
